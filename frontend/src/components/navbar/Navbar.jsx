e518b9eeaec6d45eb5c6926f8a74a9b736604a6d
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2024-10-09 22:19:08 +0530

    navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,51 +4,45 @@ import './Navbar.css'
 function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-left">
-                <div class="container flex-lg-column">
-                    <a class="navbar-brand mb-lg-2" href="#">
-                    <div className='brand-image'>Instagram</div>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-bar" aria-controls="navbar-bar" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse d-flex flex-column" id="navbar-bar">
-                        <ul class="navbar-nav flex-lg-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <nav className="navbar m-0 navbar-expand-lg bg-dark fixed-left">
+                    <div className='nav-box '>
+                        <ul className="navbar-nav flex-md-column ">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Search</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Search</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Explore</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Explore</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Reels</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Reels</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Messages</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Messages</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Notifications</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Notifications</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Create</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Create</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Profile</a>
-                            </li>
-                            <li class="nav-item mt-5">
-                                <a class="nav-link" href="#">Threads</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">More</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Profile</a>
                             </li>
                         </ul>
-                        <form class="d-flex">
+                        <ul className='navbar-nav flex-md-column nav-subox2'>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Threads</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">More</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
                         </form>
                     </div>
-                </div>
             </nav>
         </div>
     );
